93b0ef7002bf61d12c1d67a0030e49ddb97c66ea
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-27 13:58:36 -0300

    Header de la clase Cola de prioridad. Faltan heapify y encolar

--- ejercitacion_heap/src/ColaPrior.h
+++ ejercitacion_heap/src/ColaPrior.h
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-/* La clase T debe tener definido un operator<
- * que implemente una relación de orden total. */
+/* La clase T debe tener definido un operator< que implemente una relación de orden total. */
 template<class T>
 class ColaPrior {
 public:
@@ -37,6 +36,29 @@ public:
 
 private:
 	// COMPLETAR
+    vector<T> _colaPrior;
+    int _tam;
+
+    /** Retorna la posicion del hijo izquierdo dado un indice **/
+    int hijoIzquierdo(int indice);
+
+    /** Retorna la posicion del hijo derecho dado un indice **/
+    int hijoDerecho(int indice);
+
+    /** Retorna la posicion del padre dado un indice **/
+    int padre(int indice);
+
+    /** Intercambia los elementos indicados en index1 y en index2 en el vector cprior **/
+    void intercambiar(vector<T> &cprior, int index1, int index2);
+
+    /** Intercambia los elementos indicados en index1 y en index2 en el vector cprior **/
+    void siftDown(vector<T> &cprior, int index);
+
+    /** Chequea si un indice tiene hijo derecho **/
+    bool ColaPrior<T>::tieneHijoDerecho(vector<T> &cprior, int index);
+
+    /** Chequea si el indice pasado por parametro representa una hoja en la cola de prioridad **/
+    bool esHoja(vector<T> &cprior, int index);
 };
 
 #include "ColaPrior.hpp"
